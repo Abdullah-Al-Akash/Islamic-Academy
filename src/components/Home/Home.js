6ecfa8805e94141dad2443ab1e812b2ff0c75d85
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-10-04 03:04:47 +0600

    Update Slide

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Course from '../Course/Course';
+import Header from '../Header/Header';
 
 const Home = () => {
         // State Date: 
@@ -11,18 +12,22 @@ const Home = () => {
                         .then(date => setCourses(date))
         }, [])
         return (
-                <div className="container">
-                        <h2>Our Course</h2>
-                        <div class="row row-cols-1 row-cols-md-2 g-3">
-                                {
-                                        courses.map(course => <Course
-                                                course={course}
-                                                key={course.id}
-                                        >
-                                        </Course>)
-                                }
-                        </div>
+                <div>
 
+                        <Header />
+                        <div className="container">
+                                <h2>Our Course</h2>
+                                <div class="row row-cols-1 row-cols-md-2 g-3">
+                                        {
+                                                courses.map(course => <Course
+                                                        course={course}
+                                                        key={course.id}
+                                                >
+                                                </Course>)
+                                        }
+                                </div>
+
+                        </div>
                 </div>
         );
 };
