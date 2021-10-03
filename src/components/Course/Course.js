494e08d6bd4423bce3a2610911b439f327fbb08d
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-10-04 02:26:41 +0600

    Added Footer

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -6,17 +6,18 @@ const Course = (props) => {
         const { title, img, price, duration, certificate } = props.course;
         return (
                 <div className="col">
-                        <div class="card mb-3 course-card" style={{ 'maxWidth': '540px' }}>
-                                <div class="row g-0">
-                                        <div class="col-md-6 thumbnail">
-                                                <img src={img} class="img-fluid rounded-start" alt="..." />
+                        <div className="card mb-3 course-card" style={{ 'maxWidth': '540px' }}>
+                                <div className="row g-0">
+                                        <div className="col-md-6 thumbnail">
+                                                <img src={img} className="img-fluid rounded-start" alt="..." />
                                         </div>
-                                        <div class="col-md-6">
-                                                <div class="card-body">
-                                                        <h5 class="card-title text-success">{title}</h5>
-                                                        <div class="d-flex justify-content-between align-items-center">
+                                        <div className="col-md-6">
+                                                <div className="card-body">
+                                                        <h5 className="card-title text-success">{title}</h5>
+                                                        <p>Certificate: {certificate}</p>
+                                                        <div className="d-flex justify-content-between align-items-center">
                                                                 <h3>${price}</h3>
-                                                                <p>{duration}month</p>
+                                                                <p>Duration: {duration}month</p>
                                                         </div>
                                                 </div>
                                         </div>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Footer.css';
+import card from '../images/card.png';
+
+const Footer = () => {
+        return (
+                <div>
+                        <footer id="footer" className="text-white container-fluid">
+                                <div className="container pb-5">
+                                        <div className="row d-flex justify-content-between align-items-center">
+                                                <div className="col-lg-5 mt-5">
+
+                                                        <h3>Islamic Point</h3>
+
+                                                        <h6 className="mt-3"><i className="fas fa-map-marker-alt"></i> Level-4, 34, Maskanda
+                                                                Passport Office
+                                                                Road,
+                                                                Mymensingh.</h6>
+                                                        <h6><i className="fas fa-envelope"></i> Official: abc@gmail.com</h6>
+                                                        <h6><i className="fas fa-info-circle"></i> Helpline : +878545454545 (Available :
+                                                                09:00am to
+                                                                7:00pm)</h6>
+                                                </div>
+                                                <div className="col-lg-3 mt-5 footer-text">
+                                                        <h6>About Us</h6>
+                                                        <h6>Refund Policy</h6>
+                                                        <h6>Terms & Condition</h6>
+                                                        <h6>Privacy Policy</h6>
+                                                </div>
+                                                <div className="col-lg-4 mt-5">
+                                                        <div>
+                                                                <img className="img-fluid rounded" src={card} alt="" />
+                                                        </div>
+                                                </div>
+                                        </div>
+                                </div>
+                        </footer >
+                </div>
+        );
+};
+
+export default Footer;
