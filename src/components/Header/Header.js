4922cc8b22eff835bea1c6f39a40d74d186d0f6a
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-10-04 20:14:26 +0600

    Slider Update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import './Header.css';
-import HeaderImage from '../images/main2.gif';
+import HeaderImage from '../images/main-header.png';
 import About from '../About/About';
 
 const Header = () => {
         return (
                 <div>
                         <div className="header d-md-flex justify-content-center align-items-center">
-                                <div>
-                                        <img className="img-fluid" src={HeaderImage} alt="" />
-                                </div>
-                                <div className="d-md-flex align-items-center">
-                                        <div className="p-3">
-                                                <h1 class="head-title">
-                                                        Islamic Academy
-                                                </h1>
-                                                <h4 class="slogan">To Learn Quran Properly and get Proper Meaning</h4>
-                                                <p className="text-success m-0 p-0 pt-5"><i className="fas fa-sun"></i> Sunrise At: 5:10 AM</p>
-                                                <p className="text-success m-0 p-0"><i className="fas fa-moon"></i> Sunset At: 6:00 PM</p>
+                                <div className="p-3">
+                                        <h1 class="text-white text-center pt-3 pb-5">بِسْمِ ٱللّٰهِ ٱلرَّحْمٰنِ ٱلرَّحِيمِ</h1>
+                                        <h1 class="head-title">
+                                                Islamic Academy
+                                        </h1>
+                                        <h4 class="slogan">To Learn Quran Properly and get Proper Meaning <br /> With proper Tajweed, the beauty of our recitation amplifies, <br /> applying all rules and characteristics correctly.</h4>
+                                        <div className="d-flex justify-content-between text-white">
+                                                <p className="m-0 p-0 pt-5"><i className="fas fa-sun"></i> Sunrise At: 5:10 AM</p>
+                                                <p className="m-0 p-0 pt-5"><i className="fas fa-moon"></i> Sunset At: 6:00 PM</p>
                                         </div>
                                 </div>
                         </div>
