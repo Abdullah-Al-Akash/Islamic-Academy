a5213fc427dd5ea2a2f8fc5e676cc097ff707eb5
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-10-11 02:09:29 +0600

    Added Authentication Basic Functionality

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+        apiKey: "AIzaSyAbbfBp2ThxIuJIpDWJeyryI4xVkGBsxSA",
+        authDomain: "fire-authentication-123.firebaseapp.com",
+        projectId: "fire-authentication-123",
+        storageBucket: "fire-authentication-123.appspot.com",
+        messagingSenderId: "430251998396",
+        appId: "1:430251998396:web:8b214b5cb6c191fd0b24b9"
+};
+export default firebaseConfig;
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged, signOut } from "firebase/auth";
+import initializeAuthentication from "../components/Firebase/firebase.init";
+
+initializeAuthentication()
+const useFirebase = () => {
+        const [user, setUser] = useState();
+
+        // Call Auth And Provider:
+        const auth = getAuth();
+        const googleProvider = new GoogleAuthProvider();
+
+        const signInUsingGoogle = () => {
+                signInWithPopup(auth, googleProvider)
+                        .then(result => {
+                                setUser(result.user);
+                        })
+        };
+
+        const logOut = () => {
+                signOut(auth)
+                        .then(() => {
+                                setUser({});
+                        })
+        }
+
+        // Auth State Update:
+        useEffect(() => {
+                onAuthStateChanged(auth, user => {
+                        if (user) {
+                                setUser(user);
+                        }
+                })
+        }, []);
+
+        return {
+                signInUsingGoogle,
+                logOut,
+                user
+        };
+};
+
+export default useFirebase;
